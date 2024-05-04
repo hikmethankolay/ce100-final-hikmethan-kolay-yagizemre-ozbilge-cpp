8f624b9b3c3ef1fcbd4653159c90c578068a9854
hikmethankolay <hikmethan.kolay@gmail.com>
2024-05-04 17:17:53 +0300

    bug fix

--- 7-build-app-windows.bat
+++ 7-build-app-windows.bat
@@ -161,7 +161,9 @@ del test2.bin
 del test3.bin
 del test4.bin
 del test5.bin
-del usertest.bin
+del usertest1.bin
+del usertest2.bin
+del usertest3.bin
 del user.bin
 del *_records.bin
 del *_test.bin
